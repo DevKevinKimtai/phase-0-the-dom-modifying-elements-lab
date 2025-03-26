9cf3603a64ba0454fc5024c5639f5f075d0022b6
Kevin <kimutaimoringa0001@gmail.com>
2025-03-27 02:40:21 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code here!document.querySelector("main");{
+main.remove();
+const newHeader=document.createElement("H1");
+newHeader.id="victory"
+
+newHeader.innerHTML="<h1>victory</h1>masai is the champion"
